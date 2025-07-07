3ae61d36a592eb0feb1d1ff83e1cb04419ba2cf0
Lucas Tujan <lucastujan515@gmail.com>
2025-07-07 11:17:07 +0800

    Finalized app.js

--- app.js
+++ app.js
@@ -5,14 +5,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+  res.locals.url = req.path
+  next();
+});
+
+// Set up Mongoose connection
+mongoose.connect(process.env.DB);
+mongoose.promise = global.promise;
+mongoose.connection.on('error', (error) => console.error(error.message));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
