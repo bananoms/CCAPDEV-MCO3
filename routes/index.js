b147c34e6393c9cdbf91d74fbde6661e157d3a37
Lucas Tujan <lucastujan515@gmail.com>
2025-07-07 11:24:10 +0800

    Set up controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// require controllers
+
+const controller = require('../controllers/controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
