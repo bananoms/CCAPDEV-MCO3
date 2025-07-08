9330df885230ed4bcaa7b96f319722515711fd59
Lucas Tujan <lucastujan515@gmail.com>
2025-07-08 16:35:51 +0800

    started index and about pug

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ const controller = require('../controllers/controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+/* GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+})
 
 module.exports = router;
