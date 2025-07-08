5e47b18256c2ac12d2271276b5e2dfa12c91a7bd
Lucas Tujan <lucastujan515@gmail.com>
2025-07-08 17:41:48 +0800

    started sign up and log in

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,23 @@
-exports.homePage = (req,res,next) => {
+exports.homePageGet = (req,res) => {
     res.render('index');
 }
 
-exports.aboutPage = (req,res,next) => {
+exports.aboutPageGet = (req,res) => {
     res.render('about');
+}
+
+exports.loginPageGet = (req,res) => {
+    res.render('log_in');
+}
+
+exports.loginPagePost = async (req,res,next) => {
+
+}
+
+exports.signupPageGet = (req,res) => {
+    res.render('sign_up');
+}
+
+exports.signupPagePost = async (req,res,next) => {
+
 }
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ var router = express.Router();
 const controller = require('../controllers/controller');
 
 
-router.get('/', controller.homePage);
-router.get('/about', controller.aboutPage);
+router.get('/', controller.homePageGet);
+router.get('/about', controller.aboutPageGet);
+router.get('/log-in', controller.loginPageGet);
+router.post('/log-in', controller.loginPagePost);
+router.get('/sign-up', controller.signupPageGet);
+router.post('/sign-up', controller.signupPagePost);
+
 
 /* Admin Functions*/
 
