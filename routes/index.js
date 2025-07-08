eec1a0102766c2f9606d1db64365663b13c2106c
Lucas Tujan <lucastujan515@gmail.com>
2025-07-08 16:19:40 +0800

    can now be run using npm run devstart

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 // require controllers
 
 const controller = require('../controllers/controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send('hello')
 });
 
 module.exports = router;
