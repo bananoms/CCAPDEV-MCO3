678a5d5dc280355c6a4fbb1cd8c1f8c349834c11
bananoms <matthew_copon@dlsu.edu.ph>
2025-07-14 03:57:26 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,23 @@ const controller = require('../controllers/controller');
 
 
 router.get('/', controller.reservePageGet);
-router.post('/', controller.reservePagePost)
-router.get('/confirmation', controller.confirmationPageGet);
+router.post('/', controller.reservePagePost);
+router.get('/api/reservations/', controller.getReservations); // query for current reservations
+
+router.get('/confirmation/:id', controller.confirmationPageGet);
 router.get('/about', controller.aboutPageGet);
 router.get('/log-in', controller.loginPageGet);
 router.post('/log-in', controller.loginPagePost);
 router.get('/sign-up', controller.signupPageGet);
 router.post('/sign-up', controller.signupPagePost);
 
+router.get('/users/:id', controller.UserGet);
 
 /* Admin Functions*/
-
+router.get('/admin', controller.adminPageGet);
+router.get('/admin/add', controller.reservePageGet);
+router.get('/admin/edit', controller.adminEditPageGet);
+router.get('/admin/edit/:id', controller.adminEditReserve);
+router.put('/admin/edit/:id', controller.adminEditUpdate);
+router.delete('/admin/delete/:id', controller.adminDelete);
 module.exports = router;
