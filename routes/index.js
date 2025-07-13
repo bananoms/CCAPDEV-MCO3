d5749f1e45105db54991296929ce583eb68ccc94
bananoms <matthew_copon@dlsu.edu.ph>
2025-07-13 23:23:56 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,15 @@ const controller = require('../controllers/controller');
 
 
 router.get('/', controller.reservePageGet);
-router.post('/', controller.reservePagePost)
+router.post('/', controller.reservePagePost);
+router.get('/api/reservations/', controller.getReservations); // query for current reservations
+
 router.get('/confirmation', controller.confirmationPageGet);
 router.get('/about', controller.aboutPageGet);
 router.get('/log-in', controller.loginPageGet);
 router.post('/log-in', controller.loginPagePost);
 router.get('/sign-up', controller.signupPageGet);
 router.post('/sign-up', controller.signupPagePost);
-
 
 /* Admin Functions*/
 
